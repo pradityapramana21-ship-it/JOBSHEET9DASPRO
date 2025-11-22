64b38ebb52fd3ade18081a2cea836692260a7b12
radit <pradityapramana21@gmail.com>
2025-11-22 23:20:28 +0700

    shaeing modif

--- SearchNilai23.java
+++ SearchNilai23.java
@@ -1,16 +1,27 @@
+import java.util.Scanner;
 public class SearchNilai23 {
     public static void main(String[] args) {
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
-        int hasil = 0;
-    for (int i = 0; i < arrNilai.length; i++) {
-        if (key == arrNilai[i]){
-            hasil = i;
-            break;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
+        int n = sc.nextInt();
+        int[] arrNilai = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
+            arrNilai[i] = sc.nextInt();
+        }
+        System.out.print("Masukkan nilai yang ingin dicari (key): ");
+        int key = sc.nextInt();
+        int hasil = -1;
+        for (int i = 0; i < arrNilai.length; i++) {
+            if (arrNilai[i] == key) {
+                hasil = i;
+                break;
+            }
+        }
+        if (hasil != -1) {
+            System.out.println("Nilai " + key + " ditemukan di indeks ke-" + hasil);
+        } else {
+            System.out.println("Nilai " + key + " tidak ditemukan di dalam array.");
         }
     }
-    System.out.println();
-    System.out.println("Nilai "+key+" ketemu di indeks ke-"+hasil);
-    System.out.println();
-}
 }
